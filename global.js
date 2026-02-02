25d6e92e297dd73c4280674853ea18b6759deedb
qGilchrist <77083441+qGilchrist@users.noreply.github.com>
2026-02-01 21:21:14 -0700

    Resolved date issue: document.getElementById needed to be used, not jquery's id selector.

--- global.js
+++ global.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-    const year = new Date().getFullYear();
-    console.log("Current Year: "+ year);
-    $("#date").innerHTML = year;
+    const date = new Date();
+    console.log("Current Year: "+ date.getFullYear());
+    document.getElementById("date").innerHTML = date.getFullYear();
+
     getReadMe();
-}
-);
+});
 
 function getReadMe() {
     $("#readMe").load("README.md", function(response, status, xhr){
-        
-    console.log("getReadMe function log: status: "+ status+", xhr: "+xhr+" resp: "+response);
+    	console.log("getReadMe function log: status: "+ status+", xhr: "+xhr+" resp: "+response);
     });
 }
 
